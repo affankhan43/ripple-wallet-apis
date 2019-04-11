cafbcc2cad467d1d129d419b5a0850462fd17ac0
unknown <affankhan627@gmail.com>
2019-04-12 03:20:25 +0500

    first

--- ripple.php
+++ ripple.php
@@ -20,40 +20,19 @@ $address = "rpx9uJ4KgJTzaMZokPr95SznNJQv24JP52";
 $secretKey = "shdefS9eDcFp8rAL8oGPGSqJbGSpg";
 
 $ripple = new \IEXBase\RippleAPI\Ripple($address);
-$data = $ripple->getAccountTransactionHistory(null,array("limit"=>500,'start'=>"2019-04-11T20:48:50+00:00"));
+
+if(isset($_POST['msg']) && isset($_POST['key']) && isset($_POST['coin']) && $_POST['coin'] == 'xrp'){
+$data = $ripple->getAccountTransactionHistory(null,array("limit"=>500,'start'=>"2019-04-11T21:49:03+00:00"));
+$timestamp = new DateTime();
+$date = $timestamp->format('c'); // Returns ISO8601 in proper format
+$tx_data = array("transactions"=>array(),'latest'=>$date);
 for ($i=0; $i < sizeof($data); $i++) {
 	if(isset($data[$i]['hash'],$data[$i]['date'],$data[$i]['tx']['TransactionType'],$data[$i]['tx']['Amount'],$data[$i]['tx']['Destination'],$data[$i]['tx']['DestinationTag']) && $data[$i]['tx']['Destination'] == $address){
-		echo $i.') Hash---> '.$data[$i]['hash'];
-		echo "<br>";
-		echo $i.') Date---> '.$data[$i]['date'];
-		echo "<br>";
-		echo $i.') Tx Type---> '.$data[$i]['tx']['TransactionType'];
-		echo "<br>";
-		echo $i.') Amount---> '.$data[$i]['tx']['Amount']/1000000;
-		echo "<br>";
-		echo $i.') Destination---> '.$data[$i]['tx']['Destination'];
-		echo "<br>";
-		echo $i.') DestinationTag---> '.$data[$i]['tx']['DestinationTag'];
-		echo "<br>";
-		echo "<br>";
+		$tx_data['transactions'][] = array('txid'=>$data[$i]['hash'],'date'=>$data[$i]['date'],'category'=>'receive',"amount"=>$data[$i]['tx']['Amount']/1000000,"address"=>$data[$i]['tx']['Destination'],"message"=>$data[$i]['tx']['DestinationTag']);
 	}
+	$tx_data['latest'] = $data[$i]['date'];
 }
-//print_r(($data[5]));
+echo json_encode($tx_data);
 
-// $ripple = new \IEXBase\RippleAPI\Ripple($address, $secretKey);
-// try {
-//     $transfer = $ripple->buildTransaction(function (TransactionBuilderContract $item)
-//     {
-//         return $item
-//             ->setAmount(10)
-//             ->setDestinationTag(123)
-//             ->setDestination('rBiyqsFRBTFHSHaopJ5zMpS6gmsgJNAbJu')
-//             ->setTransactionType(TransactionType::PAYMENT)
-//             ->sign();
-//     })->submit();
-// } catch (Exception $e) {
-//     die($e->getMessage());
-// }
-//dump($transfer);
-
+}
 ?>
