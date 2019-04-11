bc6236ddea71509358092449815b1ef475cdba0a
unknown <affankhan627@gmail.com>
2019-04-12 00:36:09 +0500

    first

---
+++ ripple.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * ======================================================================================================
+ * File Name: ripple.php
+ * ======================================================================================================
+ * Author: affankhan43
+ *
+ * ------------------------------------------------------------------------------------------------------
+ */
+
+//$timestamp = new DateTime();
+//echo $timestamp->format('c'); // Returns ISO8601 in proper format
+//echo $timestamp->format(DateTime::ISO8601);
+
+require_once __DIR__.'/vendor/autoload.php';
+
+$address = "asd";
+$ripple = new \IEXBase\RippleAPI\Ripple($address);
+
+?>
